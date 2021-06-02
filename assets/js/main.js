b44748099f8d98da05035b4730940cb89825b234
Federica Valdemarca <61008075+FedericaValdemarca@users.noreply.github.com>
2021-06-02 16:52:08 +0200

    testimonial edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,9 +249,9 @@
   });
 
   /**
-   * Testimonials slider
+   * Collaboratori slider
    */
-  new Swiper('.testimonials-slider', {
+  new Swiper('.collaboratori-slider', {
     speed: 600,
     loop: true,
     autoplay: {
@@ -276,6 +276,37 @@
       }
     }
   });
+
+
+  /**
+   * Testimonials slider
+   */
+   new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
 
   /**
    * Animation on scroll
